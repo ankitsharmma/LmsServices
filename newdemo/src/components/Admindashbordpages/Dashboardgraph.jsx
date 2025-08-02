db930ffbe8ed67d1c98b5d66ca49a71aed7b7af8
ankitsharmma <146522409+ankitsharmma@users.noreply.github.com>
2025-08-02 14:34:44 +0530

    Update Dashboardgraph.jsx

--- newdemo/src/components/Admindashbordpages/Dashboardgraph.jsx
+++ newdemo/src/components/Admindashbordpages/Dashboardgraph.jsx
@@ -4,11 +4,13 @@ const TotalStudentBox = () => {
   const [count, setCount] = useState(0);
   const [loading, setLoading] = useState(true);
 
+ const Url="/auth/user"; // Adjust the URL as needed
+
   useEffect(() => {
-    fetch("https://api.sslcloudservices.com/auth/user")
+    fetch( `${Url}`,)
       .then((res) => res.json())
       .then((data) => {
-        console.log("Fetched students data:", data); // 👈 log here
+        console.log("Fetched students data:", data);
         setCount(data.users ? data.users.length : 0);
         setLoading(false);
       })
@@ -16,7 +18,8 @@ const TotalStudentBox = () => {
         console.error("Error fetching students:", err);
         setLoading(false);
       });
-  }, []);
+      console.log( "API URL:", Url);
+  }, [ Url]);
 
   return (
     <div className="p-4">
